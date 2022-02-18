639ff9846e96746f3d1071f70c7fc148abc74773
Animila <hristoforovila5@gmail.com>
2022-02-18 16:03:13 +0300

    Add Archer class

---
+++ UnitTests/test_archer.py
@@ -0,0 +1,33 @@
+import unittest
+from rpg.person.archer_pattern import Archer
+
+class TestArcher(unittest.TestCase):
+    '''Тесты для класса лучника'''
+
+    def test_scream(self):
+        '''проверка вывода'''
+        # ввод данных
+        test_result = 'Я лучник герой Миша'
+        test_object = Archer('Миша', 0, 0, 0, 0, 0, 0, 0)
+        test_status = test_object.scream()
+
+        # проверка
+        self.assertEqual(test_status, test_result)
+
+    def test_attach(self):
+        '''тест атаки'''
+        # ввод данных
+        test_object_1 = Archer('Миша', 100, 0, 0, 0, 0, 0, 0)
+        test_object_2 = Archer('Никита', 100, 0, 0, 0, 0, 0, 0)
+
+        # эмуляция работы
+        print('3:', test_object_1.thisExp, test_object_1.thisPower, test_object_1.thisKhack, test_object_1.thisIntel)
+        for i in range(10):
+            test_object_1.attack(test_object_2, 34)
+        print('4:', test_object_1.thisExp, test_object_1.thisPower, test_object_1.thisKhack, test_object_1.thisIntel)
+
+        # проверка
+        self.assertEqual(test_object_1.thisExp, 0)
+        self.assertEqual(test_object_1.thisPower, 1)
+        self.assertEqual(test_object_1.thisKhack, 2)
+        self.assertEqual(test_object_1.thisIntel, 4)
---
+++ person/archer_pattern.py
@@ -0,0 +1,23 @@
+from rpg.person.Hero_pattern import Hero
+
+class Archer(Hero):
+    '''Класс Лучника (наследует героя)'''
+
+    def __init__(self, name, live, mana, power, knack, intel, exp, level):
+        '''((Имя, здоровье, мана), сила, ловкость, интелект, опыт, уровень)'''
+        super(Archer, self).__init__(name, live, mana, power, knack, intel, exp, level)
+
+    def scream(self):
+        '''Вывод сообщения лучника'''
+        return f'Я лучник герой {self.thisName}'
+
+    def attack(self, target, damage):
+        '''Атака лучника'''
+        self.thisExp += 10
+        if self.thisExp == 100:
+            self.thisPower += 1
+            self.thisKhack += 2
+            self.thisIntel += 4
+            self.thisExp = 0
+        target.thisLive -= damage
+        return f'{self.thisName} нанес {target.thisName} урон {damage}'
