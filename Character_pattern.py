ad20d095062b0759b6df2fb12ce6806f24f82866
Animila <hristoforovila5@gmail.com>
2022-02-18 14:16:58 +0300

    Add Character kernel and UNIT test

---
+++ Character_pattern.py
@@ -0,0 +1,36 @@
+class Character:
+    '''Базовый персонаж'''
+
+    def __init__(self, name, live, exp):
+        '''Имя, здоровье, опыт'''
+        self.thisName = name
+        self.thisLive = live
+        self.thisExp = exp
+
+    def Status(self):
+        '''Вывод состояния базового персонажа'''
+        print('Статус персонажа:')
+        print(f'\t>Имя: {self.thisName}')
+        print(f'\t>Здоровье: {self.thisLive}')
+        print(f'\t>Мана: {self.thisExp}')
+        print('-----------------')
+
+    def drink_heal_potion(self, potion):
+        '''Проверка на ввод целых чисел для зелья здоровья'''
+        try:
+            if int(potion) == float(potion):
+                return True
+            else:
+                return 'Введено не целое число'
+        except ValueError:
+            return 'Неверный тип значения'
+
+    def drink_mana_potion(self, potion_mana):
+        '''Проверка на ввод целых чисел для зелья маны'''
+        try:
+            if int(potion_mana) == float(potion_mana):
+                return True
+            else:
+                return 'Введено не целое число'
+        except ValueError:
+            return 'Неверный тип значения'
---
+++ UnitTests/test_character.py
@@ -0,0 +1,31 @@
+import unittest
+from ..Character_pattern import Character
+
+
+class TestCharacter(unittest.TestCase):
+    '''Тесты для класса Character'''
+
+    def test_drink_heal_potion(self):
+        '''Проверка, что можно ввести только целые числа'''
+
+        # начальные данные
+        this_Object = Character('misha', 23, 45)
+        potion_1 = 2
+        potion_2 = 'a'
+        potion_3 = 9.4
+
+        # применение данных
+        potion_status_1 = this_Object.drink_heal_potion(potion_1)
+        potion_status_2 = this_Object.drink_heal_potion(potion_2)
+        potion_status_3 = this_Object.drink_heal_potion(potion_3)
+
+        # проверки:
+        # 1. Если число целое - на положительное значение
+        # 2. Если это не число - на соотвествие 'Неверный тип значения'
+        # 3. Если это не целое число - на соотвествие 'Введено не целое число'
+        self.assertTrue(potion_status_1)
+        self.assertEqual(potion_status_2, 'Неверный тип значения')
+        self.assertEqual(potion_status_3, 'Введено не целое число')
+
+if __name__ == '__name__':
+    unittest.main()
