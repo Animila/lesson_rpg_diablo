d495875a0713881af515dae1a2f24857b5158a42
Animila <hristoforovila5@gmail.com>
2022-02-18 14:18:41 +0300

    Add Warrion and UNIT test

---
+++ UnitTests/test_warrion.py
@@ -0,0 +1,61 @@
+import unittest
+from rpg.person.Warrion_pattern import Warrion
+
+
+class TestWarrion(unittest.TestCase):
+    '''Тесты для класса Воина'''
+
+    def test_UpState(self):
+        '''Проверка повышения опыта и улучшения'''
+
+        # ввод данных
+        test_object_1 = Warrion('Миша', 100, 0, 1, 1, 1, 0, 1, 'нож')
+        test_object_2 = Warrion('Денис', 100, 67, 89, 54, 56, 7, 2, 'мяч')
+
+        # эмуляция работы
+        # 1. Выводится первоначальные значения
+        # 2. Производится атака до 100 опыта
+        # 3. Выводится итоговые значения после работы
+        print(test_object_1.thisExp, test_object_1.thisPower, test_object_1.thisKhack, test_object_1.thisIntel)
+        for i in range(10):
+            test_object_1.attack(test_object_2, 33)
+        print(test_object_1.thisExp, test_object_1.thisPower, test_object_1.thisKhack, test_object_1.thisIntel)
+
+        # Проверка данных
+        self.assertEqual(test_object_1.thisPower, 4)
+        self.assertEqual(test_object_1.thisKhack, 3)
+        self.assertEqual(test_object_1.thisIntel, 2)
+        self.assertEqual(test_object_1.thisExp, 0)
+
+
+
+
+
+    def test_scream(self):
+        '''Проверка, на соотвествие значений'''
+
+        # ввод данных
+        test_result = 'Я герой Миша и я воин с нож'
+        test_object = Warrion('Миша', 100, 67, 89, 54, 56, 7, 2, 'нож')
+        test_status = test_object.scream()
+
+        # проверка на соотвествие
+        self.assertEqual(test_status, test_result)
+
+    def test_attach(self):
+        '''Проверка на атаку воина'''
+
+        # ввод данных
+        test_result = 'Миша нанес нож Денис урон 33'
+        test_object_1 = Warrion('Миша', 34, 67, 89, 54, 56, 7, 2, 'нож')
+        test_object_2 = Warrion('Денис', 100, 67, 89, 54, 56, 7, 2, 'мяч')
+        test_status = test_object_1.attack(test_object_2, 33)
+        print(test_status)
+
+        # проверка вывода строки
+        # проверка нанесения урона
+        self.assertEqual(test_status, test_result)
+        self.assertEqual(test_object_2.thisLive, 67)
+
+if __name__ == '__name__':
+    unittest.main()
---
+++ person/Warrion_pattern.py
@@ -0,0 +1,25 @@
+from rpg.person.Hero_pattern import Hero
+
+
+class Warrion(Hero):
+    '''Класс воина (наследует героя)'''
+
+    def __init__(self, name, live, mana, power, knack, intel, exp, level, weapon):
+        '''((Имя, здоровье, мана), сила, ловкость, интелект, опыт, уровень), оружие'''
+        super(Warrion, self).__init__(name, live, mana, power, knack, intel, exp, level)
+        self.thisWeapon = weapon
+
+    def scream(self):
+        '''вывод сообщения о персонаже'''
+        return f'Я герой {self.thisName} и я воин с {self.thisWeapon}'
+
+    def attack(self, target, damage):
+        target.thisLive -= damage
+        self.thisExp += 10
+        if self.thisExp == 100:
+                self.thisPower += 3
+                self.thisKhack += 2
+                self.thisIntel += 1
+                self.thisExp = 0
+        return f'{self.thisName} нанес {self.thisWeapon} {target.thisName} урон {damage}'
+
