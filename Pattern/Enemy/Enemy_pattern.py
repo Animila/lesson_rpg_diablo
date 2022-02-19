65b57a3c903d738f5935e2ebd61e0b8029684a02
Animila <hristoforovila5@gmail.com>
2022-02-19 10:00:09 +0300

    Add Enemy kernel

---
+++ Pattern/Enemy/Enemy_pattern.py
@@ -0,0 +1,16 @@
+from rpg.Pattern.Character_pattern import Character
+
+
+class Enemy(Character):
+    '''Класс противника (наследует базового персонажа)'''
+
+    def __init__(self, name, live, mana, level):
+        '''(Имя, здоровье, мана), уровень'''
+        super(Enemy, self).__init__(name, live, mana)
+        self.thisLevel = level
+        self.thisObject = []
+
+    def attack(self, target):
+        '''Атака противника на цель (на вход принимается цель)'''
+        return f'{self.thisName} атаковал {target.thisName}'
+
---
+++ UnitTests/Enemy/test_enemy.py
@@ -0,0 +1,16 @@
+import unittest
+from rpg.Pattern.Enemy.Enemy_pattern import Enemy
+
+class TestEnemy(unittest.TestCase):
+    '''Тесты для класса базового врага'''
+
+    def test_attack(self):
+        '''проверка атаки'''
+        # ввод данных
+        test_result = 'Миша атаковал Никита'
+        test_object_1 = Enemy('Миша', 100, 0, 0)
+        test_object_2 = Enemy('Никита', 100, 0, 0)
+        test_status = test_object_1.attack(test_object_2)
+
+        # проверка
+        self.assertEqual(test_status, test_result)
