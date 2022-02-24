447d10b64f4b39c03fb0e23d4efb046e8289bae0
Animila <hristoforovila5@gmail.com>
2022-02-24 21:08:06 +0300

    Refactor files

--- Game_Core.py
+++ Game_Core.py
@@ -1,33 +1,41 @@
 from Pattern.person.archer_pattern import Archer
 from Pattern.person.mag_pattern import Mag
-from Pattern.person.Warrion_pattern import Warrion
+from Pattern.person.Warrior_pattern import Warrior
 from art import logo, new_game
 import os
 
-#словарь видов персонажей
+# словарь видов персонажей
 person = {
     'маг': Mag,
     'лучник': Archer,
-    'воин': Warrion
+    'воин': Warrior
 }
 
+
 class Game:
+    """Класс запуска игры"""
+
     def __init__(self):
+        """Выполнение цели, старт"""
         self.target = False
+        self.user = ''
         self.start()
 
     def start(self):
+        """Старт игры"""
         print(logo)
         print('Добро пожаловать в нашу игру!')
-        user_name = input('Напиишите свое имя: ')
+        user_name = input('Напишите свое имя: ')
         user_var = input('Выберите класс (маг/воин/лучник): ').lower()
 
         self.user = person[user_var](name=user_name)
         self.games()
 
     def games(self):
+        """Метод самой игры"""
         os.system('\n\n\n\n\n\n\n')
         print(new_game)
+        user_var = input('Ваши действия? \n')
 
-object = Game()
 
+objects = Game()
