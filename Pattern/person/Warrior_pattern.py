ec327d801a094ffddc9dd7de07b5f9d6151e072e
Animila <hristoforovila5@gmail.com>
2022-02-24 21:02:41 +0300

    Refactor name in class warrior

--- Pattern/person/Warrior_pattern.py
+++ Pattern/person/Warrior_pattern.py
@@ -1,25 +1,25 @@
 from rpg.Pattern.person.Hero_pattern import Hero
 
 
-class Warrion(Hero):
-    '''Класс воина (наследует героя)'''
+class Warrior(Hero):
+    """Класс воина (наследует героя)"""
 
     def __init__(self, name='воин', live=100, mana=0, power=5, knack=1, intel=1, exp=0, level=1, weapon='кулак'):
-        '''((Имя, здоровье, мана), сила, ловкость, интелект, опыт, уровень), оружие'''
-        super(Warrion, self).__init__(name, live, mana, power, knack, intel, exp, level)
+        """((Имя, здоровье, мана), сила, ловкость, интелект, опыт, уровень), оружие"""
+        super(Warrior, self).__init__(name, live, mana, power, knack, intel, exp, level)
         self.thisWeapon = weapon
 
     def scream(self):
-        '''вывод сообщения о персонаже'''
+        """Вывод сообщения о персонаже"""
         return f'Я герой {self.thisName} и я воин с {self.thisWeapon}'
 
     def attack(self, target, damage):
+        """Метод атаки"""
         target.thisLive -= damage
         self.thisExp += 10
         if self.thisExp == 100:
-                self.thisPower += 3
-                self.thisKhack += 2
-                self.thisIntel += 1
-                self.thisExp = 0
+            self.thisPower += 3
+            self.thisKhack += 2
+            self.thisIntel += 1
+            self.thisExp = 0
         return f'{self.thisName} нанес {self.thisWeapon} {target.thisName} урон {damage}'
-
--- UnitTests/person/test_warrior.py
+++ UnitTests/person/test_warrior.py
@@ -1,18 +1,18 @@
 import unittest
-from rpg.Pattern.person.Warrion_pattern import Warrion
+from rpg.Pattern.person.Warrior_pattern import Warrior
 
 
-class TestWarrion(unittest.TestCase):
-    '''Тесты для класса Воина'''
+class TestWarrior(unittest.TestCase):
+    """Тесты для класса Воина"""
 
     def test_UpState(self):
-        '''Проверка повышения опыта и улучшения'''
+        """Проверка повышения опыта и улучшения"""
 
         # ввод данных
-        test_object_1 = Warrion('Миша', 100, 0, 1, 1, 1, 0, 1, 'нож')
-        test_object_2 = Warrion('Денис', 100, 67, 89, 54, 56, 7, 2, 'мяч')
+        test_object_1 = Warrior('Миша', 100, 0, 1, 1, 1, 0, 1, 'нож')
+        test_object_2 = Warrior('Денис', 100, 67, 89, 54, 56, 7, 2, 'мяч')
 
-        # эмуляция работы
+        # Эмуляция работы
         # 1. Выводится первоначальные значения
         # 2. Производится атака до 100 опыта
         # 3. Выводится итоговые значения после работы
@@ -27,28 +27,24 @@ class TestWarrion(unittest.TestCase):
         self.assertEqual(test_object_1.thisIntel, 2)
         self.assertEqual(test_object_1.thisExp, 0)
 
-
-
-
-
     def test_scream(self):
-        '''Проверка, на соотвествие значений'''
+        """Проверка, на соотвествие значений"""
 
         # ввод данных
         test_result = 'Я герой Миша и я воин с нож'
-        test_object = Warrion('Миша', 100, 67, 89, 54, 56, 7, 2, 'нож')
+        test_object = Warrior('Миша', 100, 67, 89, 54, 56, 7, 2, 'нож')
         test_status = test_object.scream()
 
-        # проверка на соотвествие
+        # проверка на соответствие
         self.assertEqual(test_status, test_result)
 
     def test_attach(self):
-        '''Проверка на атаку воина'''
+        """Проверка на атаку воина"""
 
         # ввод данных
         test_result = 'Миша нанес нож Денис урон 33'
-        test_object_1 = Warrion('Миша', 34, 67, 89, 54, 56, 7, 2, 'нож')
-        test_object_2 = Warrion('Денис', 100, 67, 89, 54, 56, 7, 2, 'мяч')
+        test_object_1 = Warrior('Миша', 34, 67, 89, 54, 56, 7, 2, 'нож')
+        test_object_2 = Warrior('Денис', 100, 67, 89, 54, 56, 7, 2, 'мяч')
         test_status = test_object_1.attack(test_object_2, 33)
         print(test_status)
 
@@ -56,6 +52,7 @@ class TestWarrion(unittest.TestCase):
         # проверка нанесения урона
         self.assertEqual(test_status, test_result)
         self.assertEqual(test_object_2.thisLive, 67)
+
 
 if __name__ == '__name__':
     unittest.main()
