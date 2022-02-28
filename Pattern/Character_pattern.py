0b4aa779773417a1365dea94126b8fa59f7b0ae7
Animila <hristoforovila5@gmail.com>
2022-02-28 17:09:15 +0300

    Я УСТАЛ! ТУТ ПРОСТО И ПОНЯТНО ВСЕ. ВСЕ!

--- Pattern/Character_pattern.py
+++ Pattern/Character_pattern.py
@@ -15,10 +15,10 @@ class Character:
         print(f'\t>Мана: {self.thisMana}')
         print('-----------------------')
 
-    def drink_heal_poion(self, potion):
-        """Проверка на ввод целых чисел для зелья здоровья"""
-        pass
+    def drink_heal_potion(self, potion):
+        """Использование зелья для восстановления жизни"""
+        self.thisLive += potion
 
     def drink_mana_potion(self, potion_mana):
-        """Проверка на ввод целых чисел для зелья маны"""
-        pass
+        """Использование зелья для восстановления манны"""
+        self.thisMana += potion_mana
--- UnitTests/test_character.py
+++ UnitTests/test_character.py
@@ -7,25 +7,14 @@ class TestCharacter(unittest.TestCase):
 
     def test_drink_heal_potion(self):
         """Проверка, что можно ввести только целые числа"""
+        test_object = Character("Misha", 100, 0)
+        test_object.drink_heal_potion(10)
+        self.assertEqual(test_object.thisLive, 110)
 
-        # начальные данные
-        this_Object = Character('misha', 23, 45)
-        potion_1 = 2
-        potion_2 = 'a'
-        potion_3 = 9.4
-
-        # применение данных
-        potion_status_1 = this_Object.drink_heal_potion(potion_1)
-        potion_status_2 = this_Object.drink_heal_potion(potion_2)
-        potion_status_3 = this_Object.drink_heal_potion(potion_3)
-
-        # Проверки:
-        # 1. Если число целое - на положительное значение
-        # 2. Если это не число - на соответствие 'Неверный тип значения'
-        # 3. Если это не целое число - на соответствие 'Введено не целое число'
-        self.assertTrue(potion_status_1)
-        self.assertEqual(potion_status_2, 'Неверный тип значения')
-        self.assertEqual(potion_status_3, 'Введено не целое число')
+    def test_drink_mana_potion(self):
+        test_object = Character("Misha", 100, 0)
+        test_object.drink_mana_potion(10)
+        self.assertEqual(test_object.thisMana, 10)
 
 
 if __name__ == '__name__':
