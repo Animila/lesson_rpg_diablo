1c50c7130758c8c6f964dfd13871ceacff3db371
Animila <hristoforovila5@gmail.com>
2022-03-11 21:07:15 +0300

    I'm tired...

--- Game_Core.py
+++ Game_Core.py
@@ -1,41 +1,127 @@
+# Импорт героев
 from Pattern.person.archer import Archer
 from Pattern.person.mag import Mag
 from Pattern.person.Warrior import Warrior
+
+# импорт NPC
+from Pattern.NPC.craftsman import Craftsman
+from Pattern.NPC.trader import Trader
+from Pattern.NPC.grasser import Greaser
+from Pattern.NPC.travel_mag import TravelMag
+
+# импорт злодеев
+from Pattern.Enemy.devil import Devil
+
+# прочие импорты
 from art import logo, new_game
-import os
 
 # словарь видов персонажей
-person = {
+person_list = {
     'маг': Mag,
+    'воин': Warrior,
     'лучник': Archer,
-    'воин': Warrior
+}
+
+# словарь видов NPC
+NPC_list = {
+    'кузнец': Craftsman(),
+    'торговец': Trader(),
+    'травник': Greaser(),
+    'маг_путник': TravelMag()
+}
+
+# словарь демонов
+devil_list = {
+    'демон': Devil()
+}
+
+# виды оружия
+armor = {
+    'меч': 30,
+    'топор': 20,
+    'лук': 10,
 }
 
 
-class Game:
-    """Класс запуска игры"""
+# локация деревни
+def village_ter(man):
+    # цикл для общей деревни
+    exit_village = False
+    while not exit_village:
+        print('Деревня')
+        various_user = input('В деревне есть кузница, рынок. Куда вы направитесь?\n').lower()
 
-    def __init__(self):
-        """Выполнение цели, старт"""
-        self.target = False
-        self.user = ''
-        self.start()
+        if various_user == 'кузница':
+            print("Кузница")
+            exit_user = False
+            while not exit_user:
+                print(' - Добрый день - поприветствовал вас кузнец, откладывая на стол молот - Вы что-то хотели?'
+                      '\nВот что я могу сделать:')
+                for element in armor:
+                    print(f'\t- {element}')
+                various_user = input('-> ').lower()
+                NPC_list['кузнец'].job(man, various_user, armor[various_user])
+                various_user = input('Что-то еще? (нет/да)\n').lower()
+                if various_user == 'нет':
+                    exit_user = True
 
-    def start(self):
-        """Старт игры"""
-        print(logo)
-        print('Добро пожаловать в нашу игру!')
-        user_name = input('Напишите свое имя: ')
-        user_var = input('Выберите класс (маг/воин/лучник): ').lower()
+        elif various_user == 'рынок':
+            print('Рынок')
+            exit_user = False
+            while not exit_user:
+                print(' - Ох, сколько лет, сколько зим - поприветствовал вас торговец, наблюдая за вами '
+                      '- Вы что-то хотели?'
+                      '\nВот что я могу сделать:')
+                # various_user = input('-> ').lower()
+                various_user = input('Что-то еще? (нет/да)\n').lower()
+                if various_user == 'нет':
+                    exit_user = True
 
-        self.user = person[user_var](name=user_name)
-        self.games()
+        elif various_user == 'выход':
+            print('Оглядываясь назад, вы еще раз прощаетесь с охранником на посту и покидаете деревню')
+            exit_village = True
 
-    def games(self):
-        """Метод самой игры"""
-        os.system('\n\n\n\n\n\n\n')
-        print(new_game)
-        user_var = input('Ваши действия? \n')
+        else:
+            print('Ошибка ввода. Повторите попытку')
 
 
-objects = Game()
+
+def scale_ter(man):
+    exit_scale = False
+    while not exit_scale:
+        print('Ущелье')
+        print('Темное ущелье скрывает от вас многие тайны. Поэтому как только вы зашли внутрь, '
+              'на вас выпрыгнуло какое-то существо')
+        various_user = input("Убежать/Напасть?").lower()
+        if various_user == "напасть":
+            print("Затычка")
+        if various_user == 'выход':
+            exit_scale = True
+
+# Начало игры
+print(logo)
+print('Добро пожаловать в нашу игру!')
+
+# Ввод данных и выбор персонажа
+user_name = input('Напишите свое имя: ')
+user_var = input('Выберите класс (маг/воин/лучник): ').lower()
+
+# Создание персонажа (имя)
+user = person_list[user_var](user_name)
+
+# Новая игры
+print(new_game)
+print(user.scream())
+print('Открыв глаза вы только что осознали - перед вами новый мир. Неизвестный новый мир.\n'
+      'Вы даже не знаете, как это произошло, потому что голова до сих пор нудит от боли.')
+print('Вы осматриваетесь по сторонам и замечаете распутье и указатель.\n'
+      'Справа находится небольшая деревня, слева - ущелье')
+user_action = input('Куда же вы направитесь? ').lower()
+
+if user_action == "направо" or user_action == 'деревня':
+    village_ter(user)
+elif user_action == 'налево' or user_action == 'ущелье':
+    scale_ter(user)
+
+
+print(user.Pack.thisPack)
